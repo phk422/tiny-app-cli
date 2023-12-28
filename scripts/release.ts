e0dd3dfbc3caa0eef9daa34a6af085b733c5fd7c
phk422 <1769476785@qq.com>
2023-12-28 10:23:37 +0800

    build(ci): 构建脚本优化

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,4 +1,4 @@
 import process from 'node:process'
 import { execSync } from 'node:child_process'
 
-execSync(`bump ${process.env.ARG} --commit "chore: release v" --tag "v" --push`, { stdio: 'inherit' })
+execSync(`bump ${process.env.VERSION_ARG} --commit "chore: release v" --tag "v" --push`, { stdio: 'inherit' })
